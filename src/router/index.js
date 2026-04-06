3565d2e6ed1e60326f67b84a75d3725df430a76d
jatinaneja <jatinaneja2000@outlook.com>
2026-04-06 18:42:04 +0100

    minor changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
-import About from '../views/About.vue';
-// import Quotes from '../views/Quotes.vue';
-import Blog from '../views/Blog.vue';
-// import Projects from '../views/Projects.vue';
-import PostDetail from '../components/blog/[slug].vue';
+// src/router/index.ts
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Blog from '../views/Blog.vue'
+import PostDetail from '../components/blog/[slug].vue'
+
 const routes = [
-    { path: '/', component: Home },
-    { path: '/about', component: About },
-    // { path: '/quotes', component: Quotes },
-    { path: '/blog', component: Blog },
-    // { path: '/projects', component: Projects },
-    {
-        // The dynamic slug route
-        path: '/blog/:slug',
-        name: 'BlogPostDetail',
-        component: PostDetail,
-      },
-];
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+  { path: '/blog', component: Blog },
+  {
+    path: '/blog/:slug',
+    name: 'BlogPostDetail',
+    component: PostDetail,
+  },
+]
+
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-    scrollBehavior(to, from, savedPosition) {
-        if (to.hash) {
-          return { selector: to.hash };
-        } else if (savedPosition) {
-          return savedPosition;
-        } else {
-          return { x: 0, y: 0 };
-        }
-      }
-});
-export default router;
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return { selector: to.hash }
+    } else if (savedPosition) {
+      return savedPosition
+    } else {
+      return { left: 0, top: 0 }
+    }
+  },
+})
+
+export default router
