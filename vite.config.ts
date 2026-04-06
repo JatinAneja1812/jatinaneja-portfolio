9fd193c9bcc5b4da7acb81d0747bb762fa4790a6
jatinaneja <jatinaneja2000@outlook.com>
2026-04-06 17:50:25 +0100

    Minor code reversion

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/jatinaneja-portfolio/about' : '/',
+  base: mode === 'production' ? '/jatinaneja-portfolio/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
